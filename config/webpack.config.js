cdd9f471ef3e336fd1a65d14fbfe55465ad981b8
DESKTOP-J3F3SIU\supOS <11111111111111111111111@163.com>
2020-11-11 19:17:58 +0800

    将webpack配置文件放入config目录下

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -20,12 +20,12 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, "../dist/"),
     publicPath: "/dist/",
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.resolve(__dirname, "../public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
     hotOnly: true
