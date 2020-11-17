267537f9fe078033d2598605c7c34c9f75db8eec
DESKTOP-J3F3SIU\supOS <11111111111111111111111@163.com>
2020-11-17 19:34:44 +0800

    feat: 增加全局环境变量

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -4,13 +4,15 @@ const pkg = require('../../package.json');
 const {merge} = require('webpack-merge')
 const common = require('./webpack.config.js')
 
+const { DEV_SERVER_PORT } = require('../env');
+
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, "../../public/"),
-        port: 8088,
+        port: DEV_SERVER_PORT,
         publicPath: "/dist/",
         hot: true,
         after(){
