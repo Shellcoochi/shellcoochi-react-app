58e3db7758698bddaf02c2e13e63ae11ea2ef7ff
DESKTOP-J3F3SIU\supOS <11111111111111111111111@163.com>
2021-03-31 15:42:40 +0800

    fix: 配置portfinder 自动寻找可用端口号

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,29 +1,42 @@
 const path = require("path");
 const webpack = require("webpack");
-const pkg = require('../../package.json');
-const {merge} = require('webpack-merge')
-const common = require('./webpack.config.js')
+const pkg = require("../../package.json");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.js");
+const portfinder = require("portfinder");
 
-const { DEV_SERVER_PORT } = require('../env');
+const { DEV_SERVER_PORT } = require("../env");
 
-
-module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, "../../public/"),
-        port: DEV_SERVER_PORT,
-        publicPath: "/dist/",
-        hot: true,
-        after(){
-            console.log(`
+const devConfig = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "../../public/"),
+    port: DEV_SERVER_PORT,
+    publicPath: "/dist/",
+    hot: true,
+    after() {
+      console.log(`
 			 
 ===================================
       ${pkg.description} 项目
 -----------------------------------
 ===================================
       `);
-        }
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
-})
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = DEV_SERVER_PORT;
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err);
+    } else {
+      devConfig.devServer.port = port;
+      console.log("项目端口号：" + port);
+      resolve(devConfig);
+    }
+  });
+});
